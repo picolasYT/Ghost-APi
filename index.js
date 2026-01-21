1fe514bfb7fc18ba3f651b7063b8d31547d7e81b
picolasYT <nicolasguevarakraan@gmail.com>
2026-01-21 17:43:57 -0300

    Intentando arreglar el .play

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 import express from "express";
-import "dotenv/config";
-import downloadRoutes from "./routes/downloader/index.js";
+import apiRoutes from "./routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
-
-// opcional, podés dejarlo si lo usás
 app.use("/files", express.static(process.cwd()));
 
-// API
-app.use("/api/download", downloadRoutes);
-
-// health check (para UptimeRobot)
-app.get("/health", (req, res) => {
-  res.json({
-    status: "ok",
-    uptime: process.uptime()
-  });
-});
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log("👻 Ghost API online en puerto", PORT);
