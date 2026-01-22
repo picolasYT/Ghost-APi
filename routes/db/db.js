93cd8da41b1e629ed9e9826b83a01cf7aff8f627
picolasYT <nicolasguevarakraan@gmail.com>
2026-01-21 22:12:39 -0300

    Initial commit - Ghost API

--- routes/db/db.js
+++ routes/db/db.js
@@ -1,11 +1,11 @@
 import pg from "pg";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({
+  path: "/etc/secrets/.env"
+});
 
 export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false
+  ssl: { rejectUnauthorized: false }
 });
