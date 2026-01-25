53e9653d3d6f1c3a25c523c28c7fea83130cb78b
picolasYT <nicolasguevarakraan@gmail.com>
2026-01-25 14:41:59 -0300

    restore admin

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 import express from "express";
+
 import tiktok from "./downloader/tiktok.js";
 import instagram from "./downloader/instagram.js";
 import youtube from "./downloader/youtube.js";
-import adminRoutes from "./routes/admin.js";
+import adminRoutes from "./admin.js";
 
 const router = express.Router();
 
+/* =========================
+   DOWNLOADERS
+========================= */
 router.use("/download/tiktok", tiktok);
 router.use("/download/instagram", instagram);
 router.use("/download/youtube", youtube);
-app.use("/admin", adminRoutes);
+
+/* =========================
+   ADMIN PANEL
+========================= */
+router.use("/admin", adminRoutes);
 
 export default router;
